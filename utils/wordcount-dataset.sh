332d0d059c3a84542d3e7c002a783fa28c7304cc
kmolski <krzysztof.molski29@gmail.com>
2023-06-06 20:11:24 +0200

    Add shell options to bash scripts

--- utils/wordcount-dataset.sh
+++ utils/wordcount-dataset.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+set -euv -o pipefail
+
 # usage: wordcount-dataset.sh <local source> <HDFS dest> <dest file bytes>
 while cat "$1"; do : ; done | head -c "$3" | hadoop fs -put - "$2"
