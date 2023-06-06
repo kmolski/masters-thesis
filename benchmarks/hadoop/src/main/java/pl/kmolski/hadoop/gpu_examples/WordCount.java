cb2110db019734e867ff827fb33d4189868449ca
kmolski <krzysztof.molski29@gmail.com>
2023-06-06 15:40:48 +0200

    Format WordCount code

--- benchmarks/hadoop/src/main/java/pl/kmolski/hadoop/gpu_examples/WordCount.java
+++ benchmarks/hadoop/src/main/java/pl/kmolski/hadoop/gpu_examples/WordCount.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.examples;
+package pl.kmolski.hadoop.gpu_examples;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
@@ -33,14 +33,12 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 public class WordCount {
 
-  public static class TokenizerMapper 
-       extends Mapper<Object, Text, Text, IntWritable>{
-    
+  public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
-      
-    public void map(Object key, Text value, Context context
-                    ) throws IOException, InterruptedException {
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
       StringTokenizer itr = new StringTokenizer(value.toString());
       while (itr.hasMoreTokens()) {
         word.set(itr.nextToken());
@@ -48,20 +46,17 @@ public class WordCount {
       }
     }
   }
-  
-  public static class IntSumReducer 
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
+
+  public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
     private IntWritable result = new IntWritable();
 
-    public void reduce(Text key, Iterable<IntWritable> values, 
-                       Context context
-                       ) throws IOException, InterruptedException {
+    public void reduce(Text key, Iterable<IntWritable> values, Context ctx) throws IOException, InterruptedException {
       int sum = 0;
       for (IntWritable val : values) {
         sum += val.get();
       }
       result.set(sum);
-      context.write(key, result);
+      ctx.write(key, result);
     }
   }
 
