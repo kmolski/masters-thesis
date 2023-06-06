1e9eb023e997b10e5b33636505eb83e8ee4461d9
kmolski <krzysztof.molski29@gmail.com>
2023-06-06 19:50:25 +0200

    Add selectable mapper for WordCount

--- benchmarks/hadoop/src/main/java/pl/kmolski/hadoop/gpu_examples/WordCount.java
+++ benchmarks/hadoop/src/main/java/pl/kmolski/hadoop/gpu_examples/WordCount.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,9 +16,6 @@
  * limitations under the License.
  */
 package pl.kmolski.hadoop.gpu_examples;
-
-import java.io.IOException;
-import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -30,55 +27,60 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+import pl.kmolski.hadoop.gpu_examples.wordcount.CpuTokenizerMapper;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
 
 public class WordCount {
 
-  public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+    private static final Map<String, Class<? extends Mapper<?, ?, ?, ?>>> MAPPERS = Map.of(
+            "cpu", CpuTokenizerMapper.class
+            //"opencl", AparapiQmcMapper.class,
+            //"cuda", JcudaQmcMapper.class
+    );
 
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
+    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
 
-    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-      StringTokenizer itr = new StringTokenizer(value.toString());
-      while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
-        context.write(word, one);
-      }
+        public void reduce(Text key, Iterable<IntWritable> values, Context ctx) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            ctx.write(key, result);
+        }
     }
-  }
 
-  public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
-    private IntWritable result = new IntWritable();
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length < 3) {
+            System.err.println("Usage: wordcount <mapper> <in> [<in>...] <out>");
+            System.exit(2);
+        }
 
-    public void reduce(Text key, Iterable<IntWritable> values, Context ctx) throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
-      }
-      result.set(sum);
-      ctx.write(key, result);
-    }
-  }
+        final String mapperName = args[0];
+        final Class<? extends Mapper<?, ?, ?, ?>> mapper =
+                Optional.ofNullable(MAPPERS.get(mapperName)).orElseThrow(
+                        () -> new IllegalArgumentException("Unknown mapper: " + mapperName)
+                );
 
-  public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-    if (otherArgs.length < 2) {
-      System.err.println("Usage: wordcount <in> [<in>...] <out>");
-      System.exit(2);
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(mapper);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        for (int i = 1; i < otherArgs.length - 1; ++i) {
+            FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
+        }
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[otherArgs.length - 1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
-    Job job = Job.getInstance(conf, "word count");
-    job.setJarByClass(WordCount.class);
-    job.setMapperClass(TokenizerMapper.class);
-    job.setCombinerClass(IntSumReducer.class);
-    job.setReducerClass(IntSumReducer.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    for (int i = 0; i < otherArgs.length - 1; ++i) {
-      FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
-    }
-    FileOutputFormat.setOutputPath(job,
-      new Path(otherArgs[otherArgs.length - 1]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
-  }
 }
