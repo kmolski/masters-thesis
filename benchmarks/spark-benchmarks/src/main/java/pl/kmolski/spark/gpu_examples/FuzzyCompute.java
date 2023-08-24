6e9c680d70bd7f4217cf489c95fa9ef1cca9f2f4
kmolski <krzysztof.molski29@gmail.com>
2023-08-24 18:02:08 +0200

    Add Spark FuzzyCompute

---
+++ benchmarks/spark-benchmarks/src/main/java/pl/kmolski/spark/gpu_examples/FuzzyCompute.java
@@ -0,0 +1,48 @@
+package pl.kmolski.spark.gpu_examples;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import pl.kmolski.spark.gpu_examples.fuzzy.CpuFuzzyComputeFunction;
+import pl.kmolski.spark.gpu_examples.fuzzy.JcudaFuzzyComputeFunction;
+import pl.kmolski.utils.SparkJobUtils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static pl.kmolski.utils.FuzzyUtils.RECORD_BYTES;
+
+public class FuzzyCompute {
+
+    private static final Map<String, FlatMapFunction<List<byte[]>, byte[]>> MAPPERS =
+            Map.of(
+                    "cpu", CpuFuzzyComputeFunction::call,
+                    "cuda", JcudaFuzzyComputeFunction::call
+            );
+
+    private static void performOperations(JavaRDD<byte[]> dataSet, String outputDir, FlatMapFunction<List<byte[]>, byte[]> mapper) {
+        SparkJobUtils.waitAndReport(() -> dataSet.glom().flatMap(mapper).saveAsObjectFile(outputDir));
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.err.printf("Usage: %s <mapper> <inputDir> <outputDir>%n", FuzzyCompute.class.getName());
+            System.exit(2);
+        }
+
+        var mapperName = args[0];
+        var mapper = Optional.ofNullable(MAPPERS.get(mapperName)).orElseThrow(
+                () -> new IllegalArgumentException("Unknown mapper: " + mapperName)
+        );
+        var inputDir = args[1];
+        var outputDir = args[2];
+
+        System.out.printf("Mapper implementation = %s%n", mapper);
+
+        try (var ctx = new JavaSparkContext()) {
+            var mapInputs = ctx.binaryRecords(inputDir, RECORD_BYTES);
+            performOperations(mapInputs, outputDir, mapper);
+        }
+    }
+}
---
+++ benchmarks/spark-benchmarks/src/main/java/pl/kmolski/spark/gpu_examples/fuzzy/CpuFuzzyComputeFunction.java
@@ -0,0 +1,16 @@
+package pl.kmolski.spark.gpu_examples.fuzzy;
+
+import pl.kmolski.utils.FuzzyUtils;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class CpuFuzzyComputeFunction {
+
+    public static Iterator<byte[]> call(List<byte[]> records) {
+        for (var record : records) {
+            FuzzyUtils.performOps(record);
+        }
+        return records.iterator();
+    }
+}
