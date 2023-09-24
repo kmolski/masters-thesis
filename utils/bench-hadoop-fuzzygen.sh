5de39425ef2db98105ac77e4872f961f91f06b1c
kmolski <krzysztof.molski29@gmail.com>
2023-09-24 13:44:04 +0200

    Update bench script for Hadoop FuzzyGen

--- utils/bench-hadoop-fuzzygen.sh
+++ utils/bench-hadoop-fuzzygen.sh
@@ -4,12 +4,15 @@ set -euv -o pipefail
 
 mkdir -p logs
 
-for n in $(seq -w 1 10); do
-    hadoop jar target/hadoop-benchmarks-1.0-SNAPSHOT-jar-with-dependencies.jar pl.kmolski.examples.FuzzyGen cpu 10000 /out/fuzzygen-cpu-10K 2>"logs/fuzzygen_cpu_10000_$n.err" | tee "logs/fuzzygen_cpu_10000_$n.out"
-    hadoop jar target/hadoop-benchmarks-1.0-SNAPSHOT-jar-with-dependencies.jar pl.kmolski.examples.FuzzyGen cpu 100000 /out/fuzzygen-cpu-100K 2>"logs/fuzzygen_cpu_100K_$n.err" | tee "logs/fuzzygen_cpu_100K_$n.out"
+JAR=target/hadoop-benchmarks-1.0-SNAPSHOT-jar-with-dependencies.jar
+MAIN_CLASS=pl.kmolski.examples.FuzzyGen
 
-    hadoop jar target/hadoop-benchmarks-1.0-SNAPSHOT-jar-with-dependencies.jar pl.kmolski.examples.FuzzyGen cuda 10000 /out/fuzzygen-cuda-10K 2>"logs/fuzzygen_cuda_10000_$n.err" | tee "logs/fuzzygen_cuda_10000_$n.out"
-    hadoop jar target/hadoop-benchmarks-1.0-SNAPSHOT-jar-with-dependencies.jar pl.kmolski.examples.FuzzyGen cuda 100000 /out/fuzzygen-cuda-100K 2>"logs/fuzzygen_cuda_100K_$n.err" | tee "logs/fuzzygen_cuda_100K_$n.out"
+for n in $(seq -w 1 4); do
+    hadoop jar "$JAR" "$MAIN_CLASS" cpu 10000 /out/fuzzygen-cpu-10K 2>"logs/fuzzygen_cpu_10000_$n.err" | tee "logs/fuzzygen_cpu_10000_$n.out"
+    hadoop jar "$JAR" "$MAIN_CLASS" cpu 100000 /out/fuzzygen-cpu-100K 2>"logs/fuzzygen_cpu_100K_$n.err" | tee "logs/fuzzygen_cpu_100K_$n.out"
+
+    hadoop jar "$JAR" "$MAIN_CLASS" cuda 10000 /out/fuzzygen-cuda-10K 2>"logs/fuzzygen_cuda_10000_$n.err" | tee "logs/fuzzygen_cuda_10000_$n.out"
+    hadoop jar "$JAR" "$MAIN_CLASS" cuda 100000 /out/fuzzygen-cuda-100K 2>"logs/fuzzygen_cuda_100K_$n.err" | tee "logs/fuzzygen_cuda_100K_$n.out"
 
     clear-hdfs-out.sh
 done
